0730354a02e2060caac9d72e6751fef97f6ea0e5
Eddy Lei <eddylei070300@gmail.com>
2021-04-18 00:14:33 +0800

    add tailwind and remove written css

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import Link from "next/link";
 import Date from "../components/date";
 import { getSortedPostsData } from '../lib/posts'
 import Layout, { siteTitle } from "../components/layout";
-import utilStyles from "../styles/utils.module.css";
 
 export default function Home({ allPostsData }) {
     return (
@@ -11,7 +10,7 @@ export default function Home({ allPostsData }) {
         <Head>
           <title>{siteTitle}</title>
         </Head>
-        <section className={utilStyles.headingMd}>
+        <section >
           <p>
             Hello, I'm <b>Eddy</b> and I like building stuffs! You can contact me
             through <a href="mailto:eddylei070300@gmail.com">email</a>
@@ -22,15 +21,15 @@ export default function Home({ allPostsData }) {
           </p>
         </section>
         <section>
-		  <h2 className={utilStyles.headingLg}>Blog</h2>
-          <ul className={utilStyles.list}>
+		  <h2 >Blog</h2>
+          <ul >
             {allPostsData.map(({ id, date, title }) => (
-              <li className={utilStyles.listItem} key={id}>
+              <li  key={id}>
                 <Link href={`/posts/${id}`}>
                     <a>{title}</a>
                 </Link>
                 <br/>
-                <small className={utilStyles.lightText}>
+                <small >
                     <Date dateString={date} />
                 </small>
               </li>
